64c23940db0d9430f5726bf7833e25c0d9db50f1
jeevavighnesh <jeevavignesh@gmail.com>
2017-09-05 09:11:06 +0530

    Hot key binding

--- dropdownScript.js
+++ dropdownScript.js
@@ -18,3 +18,16 @@ $('.dropdown-menu a').keydown(function(e){
       break;
   }
 });
+
+// define a handler
+function doc_keyUp(e) {
+
+    // this would test for whichever key is 40 and the ctrl key at the same time
+    if (e.ctrlKey && e.keyCode == 40) {
+        // call your function to do the thing
+        console.log("Key Pressed");
+        $('#t').append("Hai");
+    }
+}
+// register the handler
+document.addEventListener('keyup', doc_keyUp, false);
